a1b395e8296ef8dd180b13a0cc76bcb92b8547ce
emily-gathira <gathira.26497@students.kyu.ac.ke>
2025-10-23 18:14:53 +0300

    C program file
    converting degree to celsius

---
+++ convertToCelsius().c
@@ -0,0 +1,13 @@
+/*
+Name: Gathira Emily Wairimu
+RegNo: CT101/G/26497/25
+Description: Converting to celsius()
+*/
+
+#include<stdio.h> 
+int main() {
+	double convertToCelsius(double fahrenheit){
+		return (fahrenheit - 32.0) * (5.0 / 9.0);
+	}
+	return 0;
+}
